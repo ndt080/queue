e37c26743b781ad921caf77f5fe4df995799e3e4
NDT0 <ndt.080@yandex.by>
2020-08-02 22:47:16 +0300

    Update deploy.sh
    microupdate

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,11 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io
+#git push -f git@github.com:ndt080/ndt080.github.io.git master
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:ndt080/queue.git master:gh-pages
+
+cd -
